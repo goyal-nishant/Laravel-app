12c14d1845241d25996f69634b12fe4c5f4b643f
Nishant Goyal <nishant@gmail.com>
2024-08-08 16:18:39 +0530

    this is first commit

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+
+    <x-slot:heading>
+        Job Listings    
+    </x-slot:heading>
+
+    <ul>
+        @foreach ($jobs as $job ) 
+            <li>
+                <a href="/jobs/{{ $job['id'] }}">
+                    <strong>{{$job['title']}}:</strong> {{$job['pay']}}
+                </a>
+            </li>
+        @endforeach
+
+    </ul>
+</x-layout>
+        
---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/cache', function () {
+    return Cache::get('key');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => "Director",
+                'pay' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => "Teacher",
+                'pay' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => "Crickter",
+                'pay' => '$150,000'
+            ]
+        ]
+
+    ]);
+});
+
+Route::get('/jobs/{id}', function($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => "Director",
+            'pay' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => "Teacher",
+            'pay' => '$40,000'
+        ],
+        [
+            'id' => 3,
+            'title' => "Crickter",
+            'pay' => '$150,000'
+        ]
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
+
+});
+
+Route::get('/{id?}', function(string $id = null) {
+    return view('parameter');
+});
+
+
